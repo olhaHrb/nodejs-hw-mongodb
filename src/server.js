900509bd2cf1e396ef623667ef5cfd19109f57b4
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2025-03-03 13:35:33 +0200

    get запити

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import { getAllContacts, getContactById } from './services/contacts.js';
+import { getEnvVar } from './utils/getEnvVar.js';
 
-const PORT = 3000;
+const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
@@ -16,18 +18,44 @@ export const setupServer = () => {
     }),
   );
 
-  app.use((req, res, next) => {
-    console.log('Middleware 1');
-    next();
+  //   app.use((req, res, next) => {
+  //     console.log('Middleware 1');
+  //     next();
+  //   });
+
+  //   app.use((req, res, next) => {
+  //     console.log('Middleware 2');
+  //     next();
+  //   });
+
+  //   app.get('/', (req, res) => {
+  //     res.send('Hello world!');
+  //   });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
   });
 
-  app.use((req, res, next) => {
-    console.log('Middleware 2');
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
     next();
-  });
-
-  app.get('/', (req, res) => {
-    res.send('Hello world!');
   });
 
   app.use('*', (req, res, next) => {
--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export function getEnvVar(name, defaultValue) {
-    const value = process.env[name];
-    if (value) return value;
-    if (defaultValue) return defaultValue;
-    throw new Error(`Missing: process.env ['${name}'].`);
+  const value = process.env[name];
+  if (value) return value;
+  if (defaultValue) return defaultValue;
+  throw new Error(`Missing: process.env ['${name}'].`);
 }
