0722188b2b3a86a9cec32ea5e9f899414229abfd
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2025-03-05 13:46:17 +0200

    controllers

---
+++ src/controllers/contacts.js
@@ -0,0 +1,27 @@
+import { getContactById, getAllContacts } from '../services/contacts.js';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  if (!contact) {
+    res.status(404).json({
+      message: 'Contact not found',
+    });
+    return;
+  }
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+  next();
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,32 +1,13 @@
 import { Router } from 'express';
-import { getContactById, getAllContacts } from '../services/contacts.js';
+import {
+  getContactByIdController,
+  getContactsController,
+} from '../controllers/contacts.js';
 
 const router = Router();
 
-router.get('/contacts', async (req, res) => {
-  const contacts = await getAllContacts();
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
-});
+router.get('/contacts', getContactsController);
 
-router.get('/contacts/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-  if (!contact) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
-    return;
-  }
-  res.status(200).json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
-  });
-  next();
-});
+router.get('/contacts/:contactId', getContactByIdController);
 
 export default router;
